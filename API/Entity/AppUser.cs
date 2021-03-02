c0f9268e7bff7702840091855466e7cc89a347e0
AbdallahRizk <abm.rizk@gmail.com>
2021-03-02 23:28:24 +0300

    Added Database

---
+++ API/Entity/AppUser.cs
@@ -0,0 +1,10 @@
+namespace API.Entity
+{
+    public class AppUser
+    {
+        public int id { get; set; }
+        public string userName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
